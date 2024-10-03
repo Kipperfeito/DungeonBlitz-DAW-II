325f4b65570e8b2ee608bd18c9727c4ec9409418
Kipperfeito <heitor.k.w@gmail.com>
2024-10-03 10:59:19 -0300

    login

---
+++ usuario.js
@@ -0,0 +1,151 @@
+const db = require("../models");
+const Usuario = db.usuarios;
+const Op = db.Sequelize.Op;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const secretKey = "daw_ii_secret_key";
+
+// Criar um novo usuário
+exports.create = (req, res) => {
+  if (!req.body.usunome || !req.body.usuemail || !req.body.ususenha) {
+    res.status(400).send({
+      message: "Conteúdo não pode estar vazio",
+    });
+    return;
+  }
+
+  const usuario = {
+    usunome: req.body.usunome,
+    usuemail: req.body.usuemail,
+    usudatanascimento: req.body.usudatanascimento,
+    ususenha: req.body.ususenha,
+    usuadmin: req.body.usuadmin || 0,
+    usustatus: req.body.usustatus || 'ativo',
+  };
+
+  Usuario.create(usuario)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro ao criar usuário",
+      });
+    });
+};
+
+exports.login = (req, res) => {
+  Usuario.findOne({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then((usuario) => {
+      if (!usuario) {
+        return res.status(404).send({ message: "Usuário não encontrado." });
+      }
+      var passwordIsValid = bcrypt.compareSync(req.body.senha, usuario.senha);
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Senha inválida!",
+        });
+      }
+      var token = jwt.sign({ id: usuario.id }, secretKey, { expiresIn: "1h" });
+      res.status(200).send({ usuario: usuario, accessToken: token });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+// Buscar todos os usuários
+exports.findAll = (req, res) => {
+  const nome = req.query.nome;
+  const condition = nome ? { usunome: { [Op.iLike]: `%${nome}%` } } : null;
+
+  Usuario.findAll({ where: condition })
+    .then((data) => res.send(data))
+    .catch((err) =>
+      res.status(500).send({
+        message: err.message || "Erro ao buscar usuários",
+      })
+    );
+};
+
+// Buscar um único usuário por email
+exports.findOne = (req, res) => {
+  const email = req.params.email;
+
+  Usuario.findOne({ where: { usuemail: email } })
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Não foi possível encontrar usuário com email ${email}`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Erro ao buscar usuário com email ${email}`,
+      });
+    });
+};
+
+// Atualizar um usuário
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Usuario.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Usuário atualizado com sucesso" });
+      } else {
+        res.send({
+          message: `Não foi possível atualizar o usuário com id ${id}. Talvez o usuário não tenha sido encontrado ou req.body esteja vazio`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Erro ao atualizar o usuário com id ${id}`,
+      });
+    });
+};
+
+// Deletar um usuário
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Usuario.destroy({ where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Usuário deletado com sucesso!" });
+      } else {
+        res.send({
+          message: `Não foi possível deletar o usuário com id ${id}. O usuário não foi encontrado`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Erro ao deletar usuário com id=" + id,
+      });
+    });
+};
+
+// Deletar todos os usuários
+exports.deleteAll = (req, res) => {
+  Usuario.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} usuários foram deletados com sucesso` });
+    })
+    .catch((err) =>
+      res.status(500).send({
+        message: err.message || "Erro ao deletar usuários",
+      })
+    );
+};
